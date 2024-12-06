78c2f515342fca09726be58a4914acfac59e17e0
HuberyYang <18830279823@163.com>
2024-12-06 09:47:40 +0800

    test: test github action

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
   // 获取环境变量
   const env = loadEnv(mode, process.cwd())
   return defineConfig({
+    root: '/',
     plugins: [
       vue(),
       vueJsx(),
